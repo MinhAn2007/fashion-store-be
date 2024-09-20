e65ba8d09e07af70a156125d6aa6b5ddf7ef5f1f
minhan <voongocminhan20072002@gmail.com>
2024-09-20 09:16:37 +0700

    config vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const expressListEndpoints = require("express-list-endpoints");
-const productRoutes = require("./routes/productRoutes");
+const productRoutes = require("../routes/productRoutes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Kết nối tới cơ sở dữ liệu
-require("./config/database").connectDB(); // Kết nối với cơ sở dữ liệu MongoDB
+require("../config/database").connectDB(); // Kết nối với cơ sở dữ liệu MongoDB
 
 app.use('/api', productRoutes); // Kết nối route vào ứng dụng
 
