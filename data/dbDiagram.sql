b58141a2465a4383de858b3bb2ae61aba0e8eca9
MinhAn <voongocminhan20072002@gmail.com>
2024-09-30 18:31:56 +0700

    Revert "update schema"
    This reverts commit 7af0cd81aaad6d196ae6c1e81be2377dc5eee34a.

--- data/dbDiagram.sql
+++ data/dbDiagram.sql
@@ -19,31 +19,29 @@ CREATE TABLE Product (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     stock_quantity INT NOT NULL, -- Total available quantity
-    sold INT DEFAULT 0, -- Track quantity sold
+    sold INT,
     status TINYINT(1) NOT NULL,
     featured TINYINT(1) NOT NULL,
-    discount_rate DECIMAL(5, 2) DEFAULT 0, -- Discount percentage based on sales
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP DEFAULT NULL,
     FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE SET NULL
 );
 
--- Table for managing users
 CREATE TABLE User (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    password VARCHAR(255) NOT NULL
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- Table for managing addresses
 CREATE TABLE Address (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    user_id INT,
     address_line VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
     state VARCHAR(255) NOT NULL,
@@ -58,7 +56,7 @@ CREATE TABLE Address (
 -- Table for managing orders
 CREATE TABLE `Order` (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT NOT NULL,
+    customer_id INT,
     status ENUM('Pending Confirmation', 'In Transit', 'Delivered', 'Returned', 'Cancelled') NOT NULL,
     shipping_fee DECIMAL(18,2),
     total DECIMAL(18,2) NOT NULL,
@@ -68,7 +66,7 @@ CREATE TABLE `Order` (
     canceled_at TIMESTAMP NULL DEFAULT NULL,
     completed_at TIMESTAMP NULL DEFAULT NULL,
     delivery_at TIMESTAMP NULL DEFAULT NULL,
-    FOREIGN KEY (customer_id) REFERENCES User(id) ON DELETE CASCADE,
+    FOREIGN KEY (customer_id) REFERENCES Customer(id) ON DELETE CASCADE,
     FOREIGN KEY (payment_id) REFERENCES Payment(id) ON DELETE SET NULL,
     FOREIGN KEY (coupon_id) REFERENCES Coupon(id) ON DELETE SET NULL
 );
@@ -76,14 +74,14 @@ CREATE TABLE `Order` (
 -- Table for managing order items
 CREATE TABLE OrderItem (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
+    order_id INT,
+    product_id INT,
     name VARCHAR(255),
     quantity INT,
     price DECIMAL(10,2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP NULL DEFAULT NULL,
     FOREIGN KEY (order_id) REFERENCES `Order`(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE
 );
@@ -98,6 +96,8 @@ CREATE TABLE Coupon (
     coupon_end_date DATE,
     coupon_min_spend DECIMAL(10,2),
     coupon_max_spend DECIMAL(10,2),
+    coupon_uses_per_customer INT,
+    coupon_uses_per_coupon INT,
     coupon_status ENUM('active', 'expired', 'disabled'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -107,17 +107,17 @@ CREATE TABLE Coupon (
 -- Table for managing carts
 CREATE TABLE Cart (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT NOT NULL,
+    customer_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (customer_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES Customer(id) ON DELETE CASCADE
 );
 
 -- Table for managing items in carts
 CREATE TABLE CartItem (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    cart_id INT NOT NULL,
-    product_id INT NOT NULL,
+    cart_id INT,
+    product_id INT,
     quantity INT,
     price FLOAT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -129,8 +129,8 @@ CREATE TABLE CartItem (
 -- Table for managing product reviews
 CREATE TABLE Review (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT NOT NULL,
-    customer_id INT NOT NULL,
+    product_id INT,
+    customer_id INT,
     rating INT,
     title VARCHAR(255),
     content TEXT,
@@ -138,7 +138,7 @@ CREATE TABLE Review (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
-    FOREIGN KEY (customer_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES Customer(id) ON DELETE CASCADE
 );
 
 -- Table for managing assets (like images)
@@ -156,8 +156,8 @@ CREATE TABLE Assets (
 -- Table for managing product assets
 CREATE TABLE Product_Asset (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT NOT NULL,
-    asset_id INT NOT NULL,
+    product_id INT,
+    asset_id INT,
     type VARCHAR(255),
     FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
     FOREIGN KEY (asset_id) REFERENCES Assets(id) ON DELETE CASCADE
@@ -173,8 +173,8 @@ CREATE TABLE Attributes (
 -- Table for managing product attributes
 CREATE TABLE Product_Attribute (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT NOT NULL,
-    attribute_id INT NOT NULL,
+    product_id INT,
+    attribute_id INT,
     value VARCHAR(255),
     FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
     FOREIGN KEY (attribute_id) REFERENCES Attributes(id) ON DELETE CASCADE
@@ -183,7 +183,7 @@ CREATE TABLE Product_Attribute (
 -- Table for managing product SKUs
 CREATE TABLE products_skus (
     id INT NOT NULL AUTO_INCREMENT,
-    product_id INT NOT NULL,
+    product_id INT DEFAULT NULL,
     size_attribute_id INT DEFAULT NULL,
     color_attribute_id INT DEFAULT NULL,
     sku VARCHAR(255) NOT NULL,
@@ -191,19 +191,20 @@ CREATE TABLE products_skus (
     quantity INT NOT NULL,
     created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP NULL DEFAULT NULL,
-    image_id INT,  -- Thêm cột để liên kết với hình ảnh
     PRIMARY KEY (id),
-    FOREIGN KEY (product_id) REFERENCES Product (id) ON DELETE CASCADE,
-    FOREIGN KEY (size_attribute_id) REFERENCES Product_Attribute(id) ON DELETE SET NULL,
-    FOREIGN KEY (color_attribute_id) REFERENCES Product_Attribute(id) ON DELETE SET NULL,
-    FOREIGN KEY (image_id) REFERENCES Assets(id) ON DELETE SET NULL
+    KEY product_id (product_id),
+    KEY size_attribute_id (size_attribute_id),
+    KEY color_attribute_id (color_attribute_id),
+    CONSTRAINT  FOREIGN KEY (product_id) REFERENCES Product (id) ON DELETE CASCADE,
+    CONSTRAINT  FOREIGN KEY (size_attribute_id) REFERENCES Product_Attribute (id) ON DELETE SET NULL,
+    CONSTRAINT  FOREIGN KEY (color_attribute_id) REFERENCES Product_Attribute (id) ON DELETE SET NULL
 );
 
 -- Table for managing payment methods and discount rates
 CREATE TABLE Payment (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    payment_method ENUM('MOMO', 'CREDIT', 'PAYMENT') NOT NULL,
+    payment_method VARCHAR(255) NOT NULL,
+    discount_rate DECIMAL(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    discount_rate DECIMAL(10, 2) NOT NULL
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
