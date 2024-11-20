d80a159e2c6697446bb21f06c9961b36dcf2669a
MinhAn <voongocminhan20072002@gmail.com>
2024-11-21 00:09:28 +0700

    update verify token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const userController = require('../controllers/userControllers');
 const { verifyToken, verifyAdmin } = require('../middlewares/authMiddleware');
 
+router.get('/users', verifyAdmin, userController.getAllUsers);
+
+router.get('/users/stats', verifyAdmin, userController.getUserStats);
+
 router.get('/users/stats', userController.getUserStats);
 
 router.post('/login', userController.login);
@@ -14,20 +18,6 @@ router.get('/users/:id', verifyToken, userController.getUserInfo);
 router.put('/users/me', verifyToken, userController.updateUserInfo);
 
 router.delete('/users/me/addresses/:addressId', verifyToken, userController.deleteAddressController);
-
-//Admin
-// Lấy danh sách khách hàng với phân trang, tìm kiếm, sắp xếp
-// router.get('/users', verifyToken, userController.getAllUsers);
-//router.get('/users', userController.getAllUsers);
-router.get('/users', verifyAdmin, userController.getAllUsers);
-
-// Lấy thống kê khách hàng
-
-// router.get('/users/stats', verifyToken, userController.getUserStats);
-//router.get('/users/stats', userController.getUserStats);
-router.get('/users/stats', verifyAdmin, userController.getUserStats);
-
-
 
 //admin login
 router.post('/admin/login', userController.adminLogin);
